60a31afc421f960b8787e3cff0cbdb7d07f3cec0
Surinder Singh <surindersingh@ucreate.co.in>
2018-08-03 18:24:36 +0530

    add db connection

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+    $conn = pg_connect("host=ec2-54-217-214-201.eu-west-1.compute.amazonaws.com dbname=d8rg9krv4umqqm user=amdaovxrrzdnus password=9771ce3b5913a52b9da9e1db877e057c449ebcccf94134c0cecb2ece0f51d070 port=5432");
+
+    if($conn) {
+       echo 'connected';
+    } else {
+        echo 'there has been an error connecting';
+    } 
+
+    $create_table = pg_query($conn, "CREATE TABLE IF NOT EXISTS table1 (
+                                        id SERIAL PRIMARY KEY,
+                                        column1 CHARACTER VARYING(255) NOT NULL UNIQUE
+                                    );");
+    $insert = pg_query($conn, "INSERT INTO table1(column1) VALUES('Hello Word!')");
+
+    $select =  pg_query($conn, "Select * from table1");
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +25,13 @@
 </head>
 
 <body style="background: url(https://s3-eu-west-1.amazonaws.com/clientshare-docs/company_logo/949638008.png); color:red;">
-    Hello World!
+  <?php  print_r($select);
+    
+    echo "<br/>";
+    print_r(var_dump(pg_fetch_all($select)));
+
+    echo 'exit!';
+    ?>
 </body>
 
 </html>
