c60ad66d1a3d8d35bcbec11f3cf130b07aaad579
Surinder Singh <surindersingh@ucreate.co.in>
2018-08-22 13:37:59 +0530

    add file for send-email and logo 404 error in heroku

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php require_once('header.php');
+
+use \Rollbar\Rollbar;
+use \Rollbar\Payload\Level;
+
+Rollbar::log(Level::error(), 'Page Not Found : '$_SERVER['REQUEST_URI'].'');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>404 Page not found | Core PHP Demo</title>
+</head>
+
+<body style="color:red;">
+    <h1>Error 404</h1>
+    <p>Sorry! Page not found!</p>
+</body>
+
+</html>
---
+++ send-email.php
@@ -0,0 +1,19 @@
+<?php
+
+require('vendor/autoload.php');
+
+use Mailgun\Mailgun;
+
+# First, instantiate the SDK with your API credentials
+$mg = Mailgun::create(getenv('MAILGUN_API_KEY'));
+
+$email_data = [
+  'from'    => getenv('FROM_EMAIL'),
+  'to'      => 'surindersingh@ucreate.co.in',
+  'subject' => 'Mail Gun Email using Scheduler',
+  //'text'    => 'It is so simple to send a message.'
+  'html'    => 'Hi, <p> This email is for testing purpose. Email is sent via mailgun api by schedular ran on '.date('Y-m-d H:i:s').'. <br> You may ignore this email. </p> Regards<br>PHP DEMO Team'
+]
+# Now, compose and send your message.
+# $mg->messages()->send($domain, $params);
+$result = $mg->messages()->send(getenv('MAILGUN_DOMAIN'), $email_data);
