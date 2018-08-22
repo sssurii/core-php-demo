9bc51d122e725d022b11426d2334b051aa357e00
Surinder Singh <surindersingh@ucreate.co.in>
2018-08-22 15:38:48 +0530

    fix syntex in send-email and added phpdotenv library

--- send-email.php
+++ send-email.php
@@ -9,11 +9,13 @@ $mg = Mailgun::create(getenv('MAILGUN_API_KEY'));
 
 $email_data = [
   'from'    => getenv('FROM_EMAIL'),
-  'to'      => 'surindersingh@ucreate.co.in',
+  'to'      =>  getenv('EMAIL_TO'),
   'subject' => 'Mail Gun Email using Scheduler',
   //'text'    => 'It is so simple to send a message.'
   'html'    => 'Hi, <p> This email is for testing purpose. Email is sent via mailgun api by schedular ran on '.date('Y-m-d H:i:s').'. <br> You may ignore this email. </p> Regards<br>PHP DEMO Team'
-]
+];
 # Now, compose and send your message.
 # $mg->messages()->send($domain, $params);
 $result = $mg->messages()->send(getenv('MAILGUN_DOMAIN'), $email_data);
+
+?>
